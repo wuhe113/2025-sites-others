92ea083c0566adb4f3cc6b7c67f885f72c666e23
Helen Wu <wuz420@newschool.edu>
2025-08-29 18:36:47 -0400

    Added info toggle

--- Tracy Lin/script.js
+++ Tracy Lin/script.js
@@ -9,6 +9,8 @@ document.onmousemove = function(e){
     cursor.style.top = e.clientY + "px";
 }
 
+function changeLeftBarCursor(){
+
 leftBar.onmouseenter = function(e) {
     cursor.style.width = "200px";
     cursor.style.height = "120px";
@@ -17,4 +19,54 @@ leftBar.onmouseenter = function(e) {
 leftBar.onmouseleave = function(e) {
     cursor.style.width = originalWidth;
     cursor.style.height = originalHeight;
+};
+
+}
+
+// changeLeftBarCursor();
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    document.querySelectorAll("div").forEach(div => {
+        div.onmouseenter = function(e){
+            const hasImage = div.querySelector("img") !== null;
+            const hasText = div.textContent.trim().length > 0;
+
+            if (hasImage){
+                cursor.style.width = "200px";
+                cursor.style.height = "300px";
+            } else if(hasText){
+                cursor.style.width = "200px";
+            } else{
+                cursor.style.width = originalWidth;
+                }
+            };
+
+            div.onmouseleave = function(e){
+                cursor.style.width = originalWidth;
+            }
+     });
+});
+
+const infoButton = document.getElementById("info");
+const infoText = document.getElementById("info-full");
+
+
+// infoButton.onclick = function(event){
+//     event.preventDefault();
+
+//     let currentInfo = window.getComputedStyle(infoText).display;
+
+//     if (currentInfo === "none"){
+//         infoText.style.display = "block";
+//     } else {
+//         infoText.style.display = "none";
+//     }
+
+// }
+
+
+infoButton.onclick = function(event) {
+    event.preventDefault();
+    infoText.classList.toggle("hidden");
 };
